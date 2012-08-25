734c6d573e5a9dbb338fe462e2c67c020919c3a8
John Crisostomo <jdcrisostomo@gmail.com>
2012-08-25 15:26:41 +0800

    transfers the grav for method in usershelper

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,2 +1,7 @@
 module ProfilesHelper
+  def gravatar_for user
+    gravatar_id = Digest::MD5::hexdigest user.email
+    gravatar_url = "https://secure.gravatar.com/#{gravatar_id}"
+    image_tag(gravatar_url, alt: 'Gravatar Image', class: 'primary_pic')
+  end
 end
