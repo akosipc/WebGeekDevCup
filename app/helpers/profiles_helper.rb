af1a84acd70a2271fba74f2351b325eb8eed433a
John Crisostomo <jdcrisostomo@gmail.com>
2012-08-25 15:36:35 +0800

    downcases the email prior to hashing

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,6 +1,6 @@
 module ProfilesHelper
   def gravatar_for user
-    gravatar_id = Digest::MD5::hexdigest user.email
+    gravatar_id = Digest::MD5::hexdigest user.email.downcase
     gravatar_url = "https://secure.gravatar.com/#{gravatar_id}"
     image_tag(gravatar_url, alt: 'Gravatar Image', class: 'primary_pic')
   end
