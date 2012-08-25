0ba7b6fcc6234992f7f8debdcd150bccc0846d85
John Crisostomo <jdcrisostomo@gmail.com>
2012-08-25 16:11:48 +0800

    fixes grav issue

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,7 +1,7 @@
 module ProfilesHelper
   def gravatar_for user
     gravatar_id = Digest::MD5::hexdigest user.email.downcase
-    gravatar_url = "https://secure.gravatar.com/#{gravatar_id}"
-    image_tag(gravatar_url, alt: 'Gravatar Image', class: 'primary_pic')
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: "Profile Image", class: "profile-image")
   end
 end
